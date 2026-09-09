d698382c6c2e1ba651d785530e7a8302df63cefd
Amir Akhmetbekov <aakhmetbekov2021@fau.edu>
2026-09-09 19:36:12 -0400

    Complete Problem 2

--- Homework1/p2_Akhmetbekov_Amir.py
+++ Homework1/p2_Akhmetbekov_Amir.py
@@ -9,18 +9,18 @@ def find_Pythagorean(n):
     triples = []
 
     for a in range(1, n + 1):
-        for b in range(a + 1, n + 1):
-            for c in range(b + 1, n + 1):
+        for b in range(1, n + 1):
+            for c in range(1, n + 1):
                 if a**2 + b**2 == c**2:
                     triples.append((a, b, c))
 
     return triples
 
 
-n = int(input("Enter a positive integer n: "))
+n = int(input("Enter n: "))
 
-triples = find_Pythagorean(n)
+result = find_Pythagorean(n)
 
-for triple in triples:
-    print(triple)
+print(result)
+
     
